eb24ccfdd42130df67a29b46acd5d250fb3b7d9b
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-02-24 20:35:48 +0600

    recap of DOM elements

---
+++ recap.js
@@ -0,0 +1,26 @@
+/**
+ * 1. how to use js with html and css
+ * 
+ * 2. get element(s) from dom 
+ *  2.1. getElementById()
+ *  2.2. getElementsByClassName
+ *  2.3. querySelector()
+ *  2.4. querySelectorAll()
+ * 
+ * 3. dynamically add element to the DOM -> createElement(' ')\
+ * set value -> .innerHTML = " "
+ * 
+ * 4. get value from DOM element 
+ *      .innerText (h2, p, div, strong, span)
+ *      use value (input, textarea)
+ * 
+ * 5. add event listener (click/event handler)
+ *  5.1 click, focus, blur, onchange, keyup, keydown
+ * 
+ * 6. use function to reduce of duplicate code
+ * 
+ * 7. event delegate and event bubble 
+ */
+
+document.querySelectorAll('.user-container .disable h3');
+document.querySelectorAll('.my-users li');
